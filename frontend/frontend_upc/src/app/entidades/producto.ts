910083672119d3745b024166341e578a63b20551
ObadayaPrime <92906145+ObadayaPrime@users.noreply.github.com>
2023-08-17 21:48:53 -0400

    conexión entre el backend y frontend
    la conexion

---
+++ frontend/frontend_upc/src/app/entidades/producto.ts
@@ -0,0 +1,8 @@
+export class Producto{
+    public Producto: string = "";
+    public IdCategoria: string = "";
+    public Usuario: string = "";
+    public Fecha: string = "";
+    public Estado: string = "";
+  }
+  
